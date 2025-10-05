14f20c55c89a3282a5e7cdd893479037e851acec
amirAHS2000 <ahs.amirhosein78@gmail.com>
2025-10-05 17:26:55 +0330

    remove show_theta function because we don't want theta parameter anymore.

--- cbml_benchmark/losses/multi_prototype_cbml.py
+++ cbml_benchmark/losses/multi_prototype_cbml.py
@@ -47,8 +47,8 @@ class MultiPrototypeCBMLLoss(nn.Module):
             self.prototypes.data = prototypes
             torch.cuda.empty_cache()
 
-    def show_theta(self):
-        return self.theta.item()
+    # def show_theta(self):
+    #     return self.theta.item()
 
     def forward(self, embeddings, targets):
         # device consistency
