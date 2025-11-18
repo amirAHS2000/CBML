c8d71e6c873f2efaab68fae83a4ec35ea4d22c0d
amirAHS2000 <ahs.amirhosein78@gmail.com>
2025-11-18 13:39:02 +0330

    remove theta from the plot

--- cbml_benchmark/utils/visualization_utils.py
+++ cbml_benchmark/utils/visualization_utils.py
@@ -16,7 +16,7 @@ def plot_scalar_trends(log_path, save_dir=None):
     """
     df = pd.read_csv(log_path)
     plt.figure(figsize=(10, 6))
-    for col in ['theta', 'mvc_value', 'pos_mean', 'neg_mean', 'xi', 'main_term']:
+    for col in ['mvc_value', 'pos_mean', 'neg_mean', 'xi', 'main_term']:
         plt.plot(df['iteration'], df[col], label=col)
     plt.xlabel('Iteration')
     plt.ylabel('Value')
