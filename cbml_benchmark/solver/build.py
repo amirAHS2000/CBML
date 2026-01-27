7abcf4b8cebdb00121a206d21e073413b82e3c40
amirAHS2000 <ahs.amirhosein78@gmail.com>
2026-01-27 20:47:50 +0330

    increase training batch size + decrease backbone lr-mul

--- cbml_benchmark/solver/build.py
+++ cbml_benchmark/solver/build.py
@@ -17,7 +17,7 @@ def build_optimizer(cfg, model, criterion=None, loss_param=None):
             continue
 
         if key.startswith('backbone.'):
-            lr_mul = 0.5                     # backbone (slightly higher)
+            lr_mul = 0.3                     # backbone (slightly higher)
             weight_decay = cfg.SOLVER.WEIGHT_DECAY
         elif key.startswith('headembedding.'):
             lr_mul = 5.0                      # head learns fast
