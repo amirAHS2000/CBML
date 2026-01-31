41735bf6dd11d759ad82bd91b61a0f4530729b0b
amirAHS2000 <ahs.amirhosein78@gmail.com>
2026-01-31 20:26:26 +0330

    move to use ResNet50 again!

--- cbml_benchmark/config/model_path.py
+++ cbml_benchmark/config/model_path.py
@@ -3,7 +3,7 @@ from yacs.config import CfgNode as CN
 
 MODEL_PATH = {
     'bninception': "~/.cache/torch/checkpoints/bn_inception-52deb4733.pth",
-    'resnet18': '~/.cache/torch/hub/checkpoints/resnet18-f37072fd.pth',
+    'resnet18': '~/.cache/torch/hub/checkpoints/resnet50-19c8e357.pth',
     'googlenet': "~/.cache/torch/checkpoints/googlenet-1378be20.pth"
 }
 
