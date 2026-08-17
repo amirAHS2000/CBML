7bfaeb6dda9475119e5ac5b7fe23b2eda159ab9a
amirAHS2000 <ahs.amirhosein78@gmail.com>
2026-08-17 09:57:37 +0330

    add two config files for mobilenet backbone + general model_path

--- cbml_benchmark/config/model_path.py
+++ cbml_benchmark/config/model_path.py
@@ -2,12 +2,11 @@
 from yacs.config import CfgNode as CN
 
 MODEL_PATH = {
-    'bninception': "~/.cache/torch/hub/checkpoints/bn_inception-52deb4733.pth",
-    'resnet50': '~/.cache/torch/hub/checkpoints/resnet50-19c8e357.pth',
-    # 'resnet18': '~/.cache/torch/hub/checkpoints/resnet18-f37072fd.pth',
-    'resnet18': r'D:\Msc courses\proposal_implementation\CBML\resource\models\resnet18-f37072fd.pth',
-    'googlenet': "~/.cache/torch/hub/checkpoints/googlenet-1378be20.pth",
-    'mobilenet_v3_small': r'D:\Msc courses\proposal_implementation\CBML\resource\models\mobilenet_v3_small.pth',
+    'bninception': "resource/models/bn_inception-52deb4733.pth",
+    'resnet50': "resource/models/resnet50-19c8e357.pth",
+    'resnet18': "resource/models/resnet18-f37072fd.pth",
+    'googlenet': "resource/models/googlenet-1378be20.pth",
+    'mobilenet_v3_small': "resource/models/mobilenet_v3_small.pth",
 }
 
 MODEL_PATH = CN(MODEL_PATH)
