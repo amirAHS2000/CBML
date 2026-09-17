1d2648ff3422968d6270a638814954b7114d6df4
amirAHS2000 <ahs.amirhosein78@gmail.com>
2026-09-17 17:59:23 +0330

    add prototype initializer + caching mechanism for prototypes + necessary configs for using prototypes + add pyproject.toml (replacement for setup.py)

--- tools/main.py
+++ tools/main.py
@@ -1,4 +1,4 @@
-
+import gc
 import argparse
 import torch
 
@@ -10,7 +10,16 @@ from cbml_benchmark.modeling import build_model
 from cbml_benchmark.solver import build_lr_scheduler, build_optimizer
 from cbml_benchmark.utils.logger import setup_logger
 from cbml_benchmark.utils.checkpoint import Checkpointer
-
+from cbml_benchmark.utils.prototype_initializer import (
+    initialize_prototypes_random,
+    initialize_prototypes_mean,
+    initialize_prototypes_kmeans,
+)
+from cbml_benchmark.utils.cache_prototypes import (
+    _get_prototype_cache_path,
+    load_cached_prototypes,
+    save_prototype_cache,
+)
 
 def train(cfg):
     logger = setup_logger(name='Train', level=cfg.LOGGER.LEVEL)
@@ -23,6 +32,43 @@ def train(cfg):
     criterion_aux = None
     if cfg.LOSSES.NAME_AUX is not '':
         criterion_aux = build_aux_loss(cfg)
+
+    # initializing prototypes if using mpcbml loss
+    if cfg.LOSSES.NAME == 'cbml_loss':
+        cached_prototypes, cached_cluster_sizes, cache_path = load_cached_prototypes(cfg)
+
+        if cached_prototypes is not None:
+            logger.info(f"Loaded cached prototypes from {cache_path}, skipping recomputation.")
+            prototypes = cached_prototypes
+            cluster_sizes = cached_cluster_sizes
+        else:
+            logger.info(f"No cache found. Initializing prototypes using {cfg.LOSSES.CBML_LOSS.INIT_METHOD}...")
+
+            if cfg.LOSSES.CBML_LOSS.INIT_METHOD == 'kmeans':
+                prototypes, cluster_sizes = initialize_prototypes_kmeans(model=model, cfg=cfg)
+            elif cfg.LOSSES.CBML_LOSS.INIT_METHOD == 'mean':
+                prototypes = initialize_prototypes_mean(model=model, cfg=cfg)
+                cluster_sizes = None
+            elif cfg.LOSSES.CBML_LOSS.INIT_METHOD == 'random':
+                prototypes = initialize_prototypes_random(
+                    num_classes=cfg.LOSSES.CBML_LOSS.N_CLASSES,
+                    prototype_per_class=cfg.LOSSES.CBML_LOSS.PROTOTYPE_PER_CLASS,
+                    embed_dim=cfg.MODEL.HEAD.DIM,
+                    device=device
+                )
+                cluster_sizes = None
+            else:
+                raise ValueError(f"Unknown initializing method: {cfg.LOSSES.CBML_LOSS.INIT_METHOD}")
+
+            _, key_dict = _get_prototype_cache_path(cfg)
+            save_prototype_cache(cache_path, key_dict, prototypes, cluster_sizes)
+            logger.info(f"Saved prototype cache to {cache_path}")
+
+        criterion.set_prototypes_and_weights(prototypes, cluster_sizes)
+        del prototypes
+        torch.cuda.empty_cache()
+        gc.collect()
+        logger.info("Prototype initialization complete.")
 
     loss_param = None
     if cfg.LOSSES.NAME == 'softtriple_loss' or cfg.LOSSES.NAME == 'proxynca_loss' or cfg.LOSSES.NAME == 'center_loss' or cfg.LOSSES.NAME == 'adv_loss':
