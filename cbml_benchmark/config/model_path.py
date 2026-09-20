97e74ea65ed42f59bb0d5c05fd1231410a361142
amirAHS2000 <ahs.amirhosein78@gmail.com>
2026-09-20 23:20:47 +0330

    add resnet18 model path + comment out prototype initialization part

--- cbml_benchmark/config/model_path.py
+++ cbml_benchmark/config/model_path.py
@@ -4,7 +4,8 @@ from yacs.config import CfgNode as CN
 MODEL_PATH = {
     'bninception': "~/.cache/torch/checkpoints/bn_inception-52deb4733.pth",
     'resnet50': "~/.cache/torch/checkpoints/resnet50-19c8e357.pth",
-    'googlenet': "~/.cache/torch/checkpoints/googlenet-1378be20.pth"
+    'googlenet': "~/.cache/torch/checkpoints/googlenet-1378be20.pth",
+    'resnet18': "~/.cache/torch/checkpoints/resnet18-f37072fd.pth"
 }
 
 MODEL_PATH = CN(MODEL_PATH)
